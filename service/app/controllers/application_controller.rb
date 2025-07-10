65c480a4e37cf17c8c0463c0d4ffea8e109a9476
UTakuto <taku.0307.ue@icloud.com>
2025-07-10 09:18:11 +0900

    認証機能を追加し、現在のユーザーを設定するメソッドを実装

--- service/app/controllers/application_controller.rb
+++ service/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  before_action :set_current_user
+
+  private
+
+  def authorize_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+
+    begin
+      decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      @current_user = User.find(decoded['user_id'])
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound => e
+      render json: { errors: "認証に失敗しました: #{e.message}" }, status: :unauthorized
+    end
+  end
+
+  def set_current_user
+    @current_user = nil
+  end
 end

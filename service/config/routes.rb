21ea0572fb82ac8e7f231003d7a4a94fd2e23d32
UTakuto <taku.0307.ue@icloud.com>
2025-07-14 09:44:50 +0900

    add resources photo for show and update,destroy

--- service/config/routes.rb
+++ service/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post "/login", to: "auth#login"
   resources :users, only: [:index, :show, :create]
-  resources :photos, only: [:index, :create]
+  resources :photos, only: [:index, :show, :create, :update, :destroy]
 end

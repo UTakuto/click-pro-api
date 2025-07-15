ab46c5b7d3e8aaf7a8eddd24a8f302f4bbfbef70
UTakuto <taku.0307.ue@icloud.com>
2025-07-16 01:15:42 +0900

    ルートパスをAPIインデックスに設定

--- service/config/routes.rb
+++ service/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "api#index"
   post "/login", to: "auth#login"
   resources :users, only: [:index, :show, :create]
   resources :photos, only: [:index, :show, :create, :update, :destroy]
